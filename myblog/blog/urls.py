91e530f40d72194819881712c9b1662f096c1e34
wspjoy2011 <wspjoy2011@gmail.com>
2022-04-18 16:23:12 +0300

    Share

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -4,7 +4,8 @@ from .views import *
 app_name = 'base'
 
 urlpatterns = [
-    path('', post_list, name='post_list'),
+    path('', PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
          post_detail, name='post_detail'),
+    path('<int:post_id>/', post_share, name='post_share')
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from django.core.mail import send_mail
 
 from .models import Post
+from .forms import EmailPostForm
 
 
-def post_list(request):
-    posts = Post.objects.all()
-    context = {
-        'posts': posts,
-        'title': 'All posts'
-    }
-    return render(request, 'blog/post/list.html', context)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    extra_context = {'title': 'All posts'}
+
+
+# def post_list(request):
+#     object_list = Post.objects.all()
+#     paginator = Paginator(object_list, 3)
+#     page = request.GET.get('page')
+#
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#
+#     context = {
+#         'posts': posts,
+#         'page': page,
+#         'title': 'All posts'
+#     }
+
+    # return render(request, 'blog/post/list.html', context)
 
 
 def post_detail(request, year, month, day, post):
@@ -20,3 +44,30 @@ def post_detail(request, year, month, day, post):
         'title': 'Post detail'
     }
     return render(request, 'blog/post/detail.html', context)
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'wspjoy2011@gmail.com',
+                      [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    context = {
+        'post': post,
+        'form': form,
+        'sent': sent
+    }
+    return render(request, 'blog/post/share.html', context)
